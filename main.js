f2d4ea5e934998406cf6834dd53f3e354cc01636
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2024-02-07 20:22:08 -0300

    video presentation adjustment

---
+++ main.js
@@ -0,0 +1,12 @@
+function scrollToNextSection() {
+    const nextSection = document.querySelector('.container');
+    
+    window.scrollBy({
+        top: 920, // Descer 100px
+        behavior: 'smooth' // Suavizar a transição
+    });
+}
+
+function scrollNext() {
+    
+}
